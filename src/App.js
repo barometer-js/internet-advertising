bd50b1d8f1b5c570e7ce22e13cea72bcbf2c5e5d
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-11-28 13:59:30 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import TopNavigation from './components/TopNavigation/TopNavigation';
+import 'normalize.css';
+function App() {
+  return <TopNavigation />;
+}
+
+export default App;
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,21 @@
+function TopNavigation() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Акции</a>
+          </li>
+          <li>
+            <a href="/">О компании</a>
+          </li>
+          <li>
+            <a href="/">Контакты</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default TopNavigation;
