f86fa79aa024d7f7506a3a24ecd27893f8f8a06e
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-03 22:45:12 +0200

    added styles

--- src/components/TopNavigation/TopNavigation.js
+++ src/components/TopNavigation/TopNavigation.js
@@ -1,19 +1,17 @@
 import HeaderContacts from '../Contacts/HeaderContacts';
+import s from './TopNavigation.module.scss';
+import dataTopNav from './dataTopNav.json';
 
 function TopNavigation() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Акции</a>
-          </li>
-          <li>
-            <a href="/">О компании</a>
-          </li>
-          <li>
-            <a href="/">Контакты</a>
-          </li>
+    <div className={s.section}>
+      <nav className={s.navigation}>
+        <ul className={s.list}>
+          {dataTopNav.map(data => (
+            <li className={s.item} key={data.id}>
+              <a href="/">{data.title}</a>
+            </li>
+          ))}
         </ul>
       </nav>
       <HeaderContacts />
