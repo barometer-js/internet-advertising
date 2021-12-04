022851eae4c2277cc0fdf13b364abf2950c6d737
ViktoriiaPavlenko <vktr9999@ukr.net>
2021-12-04 21:28:10 +0200

    form styles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -59,86 +59,95 @@ class ApplicationForm extends Component {
   render() {
     return (
       <form className={styles.form} onSubmit={this.handleSubmit}>
-        <label className={styles.label} htmlFor={this.nameId}>
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            value={this.state.name}
-            placeholder="Name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-            id={this.nameId}
-          />
-        </label>
+        {/* <b className={styles.title}>Форма заявки</b> */}
+        <div className={styles.wrapper}>
+          <label className={styles.label} htmlFor={this.nameId}>
+            <input
+              className={styles.input}
+              type="text"
+              name="name"
+              value={this.state.name}
+              placeholder="Name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleChange}
+              id={this.nameId}
+            />
+          </label>
 
-        {/* <label className={styles.label} htmlFor={this.surnameId}>
-          <input
-            className={styles.input}
-            type="text"
-            name="surname"
-            value={this.state.surname}
-            placeholder="Surname"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Surname may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-            id={this.surnameId}
-          />
-        </label> */}
+          {/* <label className={styles.label} htmlFor={this.surnameId}>
+            <input
+              className={styles.input}
+              type="text"
+              name="surname"
+              value={this.state.surname}
+              placeholder="Surname"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Surname may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleChange}
+              id={this.surnameId}
+            />
+          </label> */}
 
-        <label className={styles.label} htmlFor={this.telId}>
-          <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            value={this.state.number}
-            placeholder="Number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-            id={this.telId}
-          />
-        </label>
+          <label className={styles.label} htmlFor={this.telId}>
+            <input
+              className={styles.input}
+              type="tel"
+              name="number"
+              value={this.state.number}
+              placeholder="Number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleChange}
+              id={this.telId}
+            />
+          </label>
+        </div>
 
-        <label className={styles.label} htmlFor={this.emailId}>
-          <input
-            className={styles.input}
-            type="email"
-            name="email"
-            value={this.state.email}
-            placeholder="Email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-            title="Invalid email address"
-            required
-            onChange={this.handleChange}
-            id={this.emailId}
-          />
-        </label>
+        <div className={styles.wrapper}>
+          <label className={styles.label} htmlFor={this.emailId}>
+            <input
+              className={styles.input}
+              type="email"
+              name="email"
+              value={this.state.email}
+              placeholder="Email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              title="Invalid email address"
+              required
+              onChange={this.handleChange}
+              id={this.emailId}
+            />
+          </label>
 
-        <label>
-          <textarea
-            value={this.state.value}
-            onChange={this.handleTextareaChange}
-          />
-        </label>
+          <label>
+            <textarea
+              className={styles.textarea}
+              value={this.state.value}
+              onChange={this.handleTextareaChange}
+            />
+          </label>
+        </div>
 
-        <label>
-          <Checkbox
-            checked={this.state.checked}
-            onChange={this.handleCheckboxChange}
-          />
-          <span>
-            Нажимая на кнопку, вы соглашаетесь с нашей политикой обработки
-          </span>
-        </label>
+        <div className={styles.wrapper}>
+          <label>
+            <Checkbox
+              checked={this.state.checked}
+              onChange={this.handleCheckboxChange}
+            />
+            <span className={styles.policy}>
+              Нажимая на кнопку, вы соглашаетесь <br /> с нашей политикой
+              обработки
+            </span>
+          </label>
 
-        <button className={styles.button} type="submit">
-          Send
-        </button>
+          <button className={styles.button} type="submit">
+            Send
+          </button>
+        </div>
       </form>
     );
   }
