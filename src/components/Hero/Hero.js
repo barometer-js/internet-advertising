b86a90cda058fbbf9ba349e74f1825443000a1f9
Anastasiya-Baranouskaya <nastenkabv@gmail.com>
2021-12-19 18:53:57 +0100

    fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,16 +4,16 @@ import styled from 'styled-components';
 const Title = styled.h1`
   display: flex;
   justify-content: center;
-
   font-weight: 700;
   font-size: 55px;
   line-height: 1;
   text-transform: uppercase;
-  //   .background1 {
-  //     background-image: linear-gradient(#00000033, #00000033),
-  //       url('../../images/backgroundHero1x.png');
-  //   }
 `;
+
+//   .background1 {
+//     background-image: linear-gradient(#00000033, #00000033),
+//       url('../../images/backgroundHero1x.png');
+//   }
 
 export default function Hero() {
   return (
--- src/components/Section.js
+++ src/components/Section.js
@@ -5,6 +5,10 @@ const SectionWrapper = styled.section`
   background-color: #333333;
   padding-right: 100px;
   padding-left: 100px;
+  .background1 {
+    background-image: linear-gradient(#00000033, #00000033),
+      url('../../images/backgroundHero1x.png');
+  }
 `;
 export function Section({ children }) {
   return (
