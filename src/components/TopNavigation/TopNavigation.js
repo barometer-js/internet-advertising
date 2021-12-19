1fa810e73a366e30232a056d5e154a844e4ca07e
Vitaliy-Karpenko529 <vkarpenko945@gmail.com>
2021-12-19 13:36:02 +0200

    added styles navigation

--- src/components/TopNavigation/TopNavigation.js
+++ src/components/TopNavigation/TopNavigation.js
@@ -1,10 +1,45 @@
 import HeaderContacts from '../Contacts/HeaderContacts';
+import styled from 'styled-components';
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const NavList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  li {
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 1.2;
+    letter-spacing: 0.04em;
+    text-transform: uppercase;
+    color: #333333;
+    a {
+      color: inherit;
+      padding-top: 18px;
+      padding-bottom: 18px;
+      display: inline-block;
+    }
+  }
+  li:not(:last-child):after {
+    content: '/';
+    margin-left: 25px;
+    margin-right: 25px;
+    display: block;
+  }
+`;
 
 function TopNavigation() {
   return (
-    <div>
+    <NavWrapper>
       <nav>
-        <ul>
+        <NavList>
           <li>
             <a href="/">Акции</a>
           </li>
@@ -14,10 +49,10 @@ function TopNavigation() {
           <li>
             <a href="/">Контакты</a>
           </li>
-        </ul>
+        </NavList>
       </nav>
       <HeaderContacts />
-    </div>
+    </NavWrapper>
   );
 }
 
