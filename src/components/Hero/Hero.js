26c455ac8118e2874b94074108f8bb9c061b8bbb
Anastasiya-Baranouskaya <nastenkabv@gmail.com>
2021-12-20 22:11:56 +0100

    delete coment

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,5 @@
 import { Section } from '../Section';
 import styled from 'styled-components';
-
 const Title = styled.h1`
   display: flex;
   justify-content: center;
@@ -9,13 +8,6 @@ const Title = styled.h1`
   line-height: 1;
   text-transform: uppercase;
 `;
-
-//   .background1 {
-//     background-image:
-//  linear - gradient (#00000033, #00000033),
-//       url('../../images/backgroundHero1x.png');
-//   }
-
 export default function Hero() {
   return (
     <Section className="background1">
